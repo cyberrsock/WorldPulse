292b81c8839ff25abdbe3f6ad070c20a0f9f5374
Dmitry <91433045+DiZH6414@users.noreply.github.com>
2025-04-10 21:29:53 +0300

    WPDEV-16: some more parser and mongo save

---
+++ services/news-parser/src/realization/MongoDBManager.py
@@ -0,0 +1,66 @@
+from bson.binary import Binary
+from bson import ObjectId
+from datetime import datetime
+from typing import Dict, List
+import pymongo
+import os
+from urllib.parse import quote
+
+
+class MongoDBManager:
+    def __init__(self):
+        self._url = "mongodb://{user}:{pw}@{hosts}/?replicaSet={rs}&authSource={auth_src}".format(
+            user=quote("wpdev"),
+            pw=quote(os.getenv("MONGO_PASSWORD")),
+            hosts=",".join([os.getenv("MONGO_HOST")]),
+            rs="rs01",
+            auth_src="worldpulse",
+        )
+
+    def _get_connection(self):
+        return pymongo.MongoClient(
+            self._url, tls=True, tlsCAFile=".mongodb/root.crt"
+        )
+
+    def add_news(self, channel: str, msg_id: int, msg: str, time: str):
+        doc = {
+            "channel": channel,
+            "msg_id": msg_id,
+            "content": msg,
+            "datetime": datetime.fromisoformat(time) if isinstance(time, str) else time,
+        }
+        with self._get_connection() as client:
+            db = client["worldpulse"]
+            db["news"].insert_one(doc)
+
+    def add_or_update_clusterized_news(self, cluster: Dict):
+        with self._get_connection() as client:
+            db = client["worldpulse"]
+            collection = db["clusterized_news"]
+
+            cluster_id = cluster["id"]
+            if cluster_id == -1:
+                # создаём новый кластер
+                doc = {
+                    "description": cluster["text"],
+                    "embedding": Binary(cluster["embedding"].encode("utf-8")),
+                    "classes": cluster["classes"],
+                    "news_ids": [cluster["msg_id"]],
+                    "first_time": datetime.fromisoformat(cluster["time"]),
+                    "last_time": datetime.fromisoformat(cluster["time"]),
+                }
+                collection.insert_one(doc)
+            else:
+                # обновляем существующий кластер
+                collection.update_one(
+                    {"_id": ObjectId(cluster_id)},
+                    {
+                        "$set": {
+                            "description": cluster["text"],
+                            "embedding": Binary(cluster["embedding"].encode("utf-8")),
+                            "classes": cluster["classes"],
+                            "last_time": datetime.fromisoformat(cluster["time"]),
+                        },
+                        "$addToSet": {"news_ids": cluster["msg_id"]},
+                    }
+                )
